e2791467c16d2bb4fd39f3d6aea77acbe514355c
Fadseres <evteev.mikhail.a@gmail.com>
2020-03-19 17:26:47 +0300

    Коммит

--- cloud-client/src/main/java/client/ControllerCL.java
+++ cloud-client/src/main/java/client/ControllerCL.java
@@ -3,17 +3,27 @@ package client;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerCL {
+
+public class ControllerCL implements Initializable {
 
     @FXML
-    ListView filesList;
+    ListView<String> filesList;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        filesList.getItems().addAll("Java", "Core", "EX", "Qwerty");
+    }
 
 
 
     public void menuItemFileExitAction(ActionEvent actionEvent) {
         Platform.exit();
     }
+
+
 }
