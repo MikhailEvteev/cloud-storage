ca176caf3fbd1970d5bbde2176e77a2b3834591e
Mikhail <evteev.mikhail.a@gmail.com>
2020-04-05 18:36:59 +0300

    Соединение сервера

--- cloud-server/src/main/java/server/ServerApp.java
+++ cloud-server/src/main/java/server/ServerApp.java
@@ -1,41 +1,44 @@
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import server.ServerHandler;
 
 public class ServerApp {
+    private static final int PORT = 8189;
 
-    public static void main(String[] args) throws Exception {
-        new ServerApp().run();
+    public ServerApp() {
     }
 
-     public void run() throws Exception {
-        EventLoopGroup bossCroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
-
-        ServerBootstrap bootstrap = new ServerBootstrap();
-        bootstrap.group(bossCroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
-
-                    }
-                });
-            ChannelFuture future = bootstrap.bind(8189).sync();
+            ServerBootstrap b = new ServerBootstrap();
+            ((ServerBootstrap)b.group(bossGroup, workerGroup).
+                    channel(NioServerSocketChannel.class)).
+                    childHandler(new ChannelInitializer<SocketChannel>() {
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new ChannelHandler[]{
+                            new StringDecoder(), new StringEncoder(), new ServerHandler()
+                    });
+                }
+            });
+            ChannelFuture future = b.bind(8189).sync();
             future.channel().closeFuture().sync();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception var8) {
+            var8.printStackTrace();
         } finally {
-            bossCroup.shutdownGracefully();
-            bossCroup.shutdownGracefully();
-
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
+
     }
 }
--- cloud-server/src/main/java/server/ServerHandler.java
+++ cloud-server/src/main/java/server/ServerHandler.java
@@ -1,31 +1,65 @@
 package server;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.*;
 
-public class ServerHandler extends ChannelInboundHandlerAdapter {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("Клиент подключился");
-        super.channelActive(ctx);
+public class ServerHandler extends SimpleChannelInboundHandler<String> {
+    private static final List<Channel> channels = new ArrayList();
+    private static int newClientIndex = 1;
+    private String clientName;
+
+    public ServerHandler() {
     }
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Клиент подключился: " + ctx);
+        channels.add(ctx.channel());
+        this.clientName = "Клиент #" + newClientIndex;
+        ++newClientIndex;
+        this.broadcastMessage("SERVER", "Подключился новый клиент: " + this.clientName);
+    }
 
-        ByteBuf buf = (ByteBuf)msg;
-        while (buf.readableBytes() > 0) {
-            System.out.println((char) buf.readByte());
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+        System.out.println("Получено сообщение: " + s);
+        if (s.startsWith("/")) {
+            if (s.startsWith("/changename ")) {
+                String newNickname = s.split("\\s", 2)[1];
+                this.broadcastMessage("SERVER", "Клиент " + this.clientName + " сменил ник на " + newNickname);
+                this.clientName = newNickname;
+            }
+
+        } else {
+            this.broadcastMessage(this.clientName, s);
         }
     }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        cause.printStackTrace();
-        ctx.close();
+    public void broadcastMessage(String clientName, String message) {
+        String out = String.format("[%s]: %s\n", clientName, message);
+        Iterator var4 = channels.iterator();
+
+        while(var4.hasNext()) {
+            Channel c = (Channel)var4.next();
+            c.writeAndFlush(out);
+        }
 
     }
+
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Клиент " + this.clientName + " вышел из сети");
+        channels.remove(ctx.channel());
+        this.broadcastMessage("SERVER", "Клиент " + this.clientName + " вышел из сети");
+        ctx.close();
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("Клиент " + this.clientName + " отвалился");
+        channels.remove(ctx.channel());
+        this.broadcastMessage("SERVER", "Клиент " + this.clientName + " вышел из сети");
+        ctx.close();
+    }
 }
+
