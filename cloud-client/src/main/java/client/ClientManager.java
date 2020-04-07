523d9e9c6fbb58e8ae794bfe8d47f2545bf3d21c
Mikhail <evteev.mikhail.a@gmail.com>
2020-04-07 20:48:27 +0300

    Доработка 2

--- cloud-client/src/main/java/client/ClientManager.java
+++ cloud-client/src/main/java/client/ClientManager.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ClientCL extends Application {
+public class ClientManager extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/manager.fxml"));
--- cloud-client/src/main/java/client/FileInfo.java
+++ cloud-client/src/main/java/client/FileInfo.java
@@ -1,4 +1,39 @@
 package client;
 
+import java.io.File;
+
+
 public class FileInfo {
+    private File file;
+    private String name;
+    private long length;
+
+    public String getFilename() {
+        return name;
+    }
+
+    public long getLength() {
+        return length;
+    }
+
+    public void setFilename(String filename) {
+        this.name = name;
+    }
+
+    public void setLength(long length) {
+        this.length = length;
+    }
+
+    public FileInfo(File file) {
+        this.file = file;
+        this.name = file.getName();
+        this.length = file.length();
+
+    }
+
+    public FileInfo(File file, String name, long length) {
+        this.file = file;
+        this.name = name;
+        this.length = length;
+    }
 }
